87fdde2f908c56479ee161ef340bf28fe55390c9
Ethan Kieser <ethankieser@gmail.com>
2020-03-18 16:20:30 -0500

    updated features

--- src/Features.js
+++ src/Features.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 function Features(props) {
-    const features = Object.keys(this.props.features)
+    const features = Object.keys(props.features)
     .map(key => {
-        const options = this.props.features[key].map((item, index) => {
-            const selectedClass = item.name === this.props.selected[key].name ? 'feature__selected' : '';
-            const featureClass = 'feature__option' + selectedClass;
+        const options = props.features[key].map((item, index) => {
+            const selectedClass = item.name === props.selected[key].name ? 'feature__selected' : '';
+            const featureClass = 'feature__option' + ' ' + selectedClass;
             return (
                 <li key={index} className="feature__item">
                     <div className={featureClass}
-                        onClick={e => this.props.handleUpdate(key, item)}>
+                        onClick={e => props.handleUpdate(key, item)}>
                         { item.name }
                         ({ new Intl.NumberFormat( 'en-US', {style: 'currency', currency: 'USD'})
                         .format(item.cost)})
